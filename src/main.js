b9882ffa3ea3cd41af6c66c3f6b8a9bf7feb6f93
Ваня <7328652@gmail.com>
2024-03-03 18:20:42 +0200

    fix swapper

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import './js/hero-modal.js';
-
 import './js/mobile-menu.js';
-import './js/swiper.js';
 import './js/reviews-flip-card.js';
 import './js/hoverla-modal.js';
 import './js/bukovel-modal.js';
 import './js/carpathians-modal.js';
 import './js/tour.js';
+import './js/swiper.js';
