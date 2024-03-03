52db3b46f7f483233a76b76012d72f8ace5a1142
Ваня <7328652@gmail.com>
2024-03-03 17:36:28 +0200

    fixed errors with path

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './js/hero-modal.js';
+
+import './js/mobile-menu.js';
+import './js/swiper.js';
+import './js/reviews-flip-card.js';
+import './js/hoverla-modal.js';
+import './js/bukovel-modal.js';
+import './js/carpathians-modal.js';
+import './js/tour.js';
